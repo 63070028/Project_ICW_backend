6c6c23f60b118baa048af7f14cb0a85375e53a73
63070078 <63070078@kmitl.acath>
2023-04-15 00:09:43 +0700

    ploy ud

--- routes/company.js
+++ routes/company.js
@@ -8,7 +8,6 @@ const { uploadToS3 } = require("../s3");
 const multer = require("multer");
 const upload = multer();
 
-
 router.post("/signUp", async (req, res) => {
   //database
   const params = {
@@ -37,42 +36,77 @@ router.post("/signUp", async (req, res) => {
   }
 });
 
-router.post("/edit",upload.fields([{ name: "profile_image", maxCount: 1 }, { name: "background_image", maxCount: 1 },]),async (req, res) => {
+router.post("/edit",upload.fields([
+    { name: "profile_image", maxCount: 1 },
+    { name: "background_image", maxCount: 1 },
+  ]),
+  async (req, res) => {
     console.log(req.body);
-    console.log(req.files)
+    console.log(req.files);
     const profileImageFile = req.files["profile_image"]?.[0];
     const backgroundImageFile = req.files["background_image"]?.[0];
     let profileImageUrl = "";
     let backgroundImageUrl = "";
     try {
       if (profileImageFile) {
-        const FileName = `${req.body.company_id}-${uuidv4()}-${
-          profileImageFile.originalname
-        }`;
-        const FolderS3 = "images"; // เปลี่ยน FolderS3 เป็น "images"
+        const fileName = `${req.body.id}-${uuidv4()}-${profileImageFile.originalname
+          }`;
+        const folderS3 = "images";
         profileImageUrl = await uploadToS3(
           `${process.env.S3_BUCKET}`,
-          FileName,
-          FolderS3,
+          fileName,
+          folderS3,
           profileImageFile
         );
         console.log("Profile image uploaded to:", profileImageUrl);
       }
-      
       if (backgroundImageFile) {
-        const FileName = `${req.body.company_id}-${uuidv4()}-${
-          backgroundImageFile.originalname
-        }`;
-        const FolderS3 = "background_images";
+        const fileName = `${req.body.id}-${uuidv4()}-${backgroundImageFile.originalname
+          }`;
+        const folderS3 = "background_images";
         backgroundImageUrl = await uploadToS3(
           `${process.env.S3_BUCKET}`,
-          FileName,
-          FolderS3,
+          fileName,
+          folderS3,
           backgroundImageFile
         );
         console.log("Background image uploaded to:", backgroundImageUrl);
       }
-      
+      const params = {
+        TableName: "company",
+        Key: {
+          id: { S: req.body.id },
+        },
+        UpdateExpression: `
+        name = :name,
+        description = :description,
+        ${profileImageUrl ? "profile_image = :profile_image," : ""}
+        ${backgroundImageUrl ? "background_image = :background_image," : ""}
+        video_iframe = :video_iframe,
+        state = :state
+      `,
+      ExpressionAttributeValues: {
+
+        ":name": { S: req.body.name },
+        ":description": { S: req.body.description},
+        ":profile_image": {S: profileImageUrl},
+        ":background_image": { S: backgroundImageUrl},
+        ":video_iframe": { S: req.body.video_iframe },
+        ":state": { S: req.body.state },
+        }
+      };
+
+      try {
+        await updateItem(params);
+        res.status(201).json({
+          message: "Company profile updated successfully",
+          ...(profileImageUrl ? { profile_image: profileImageUrl } : {}),
+          ...(backgroundImageUrl ? { background_image: backgroundImageUrl } : {}),
+        });
+      } catch (error) {
+        console.error(error);
+        res.status(500).send(error.message);
+      }
     } catch (error) {
       console.error(error);
       res.status(500).send(error.message);
