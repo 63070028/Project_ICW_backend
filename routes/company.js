659e3774a77e417e0aa28911e073fec0d2bb350b
tnploy <63070078@kmitl.ac.th>
2023-04-23 14:59:19 +0700

    Update company.js

--- routes/company.js
+++ routes/company.js
@@ -174,11 +174,11 @@ router.post("/editJob", async (req, res) => {
         "SET #n = :name, #c = :capacity, #l = :location, detail = :detail, interview = :interview, salary_per_day = :salary_per_day, #s = :state, qualifications = :qualifications",
       ExpressionAttributeValues: {
         ":name": { S: req.body.name },
-        ":capacity": { N: req.body.capacity  },
+        ":capacity": { N: req.body.capacity.toString()  },
         ":location": { S: req.body.location },
         ":detail": { S: req.body.detail },
         ":interview": { S: req.body.interview },
-        ":salary_per_day": { N: req.body.salary_per_day },
+        ":salary_per_day": { N: req.body.salary_per_day.toString()  },
         ":state": { S: req.body.state },
         ":qualifications": {
           SS:
